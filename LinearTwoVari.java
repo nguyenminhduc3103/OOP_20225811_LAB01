5206bf756ff8b4173bdace71c8a675d488f380aa
MD31032004 <ngminhduc04@gmail.com>
2024-09-28 16:09:10 +0700

    fix

--- LinearTwoVari.java
+++ LinearTwoVari.java
@@ -5,26 +5,26 @@ public class LinearTwoVari{
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Nhap so a_11: ");
-        int a_11 = scanner.nextInt();
+        double a_11 = scanner.nextDouble();
 
         System.out.print("Nhap so a_12: ");
-        int a_12 = scanner.nextInt();
+        double a_12 = scanner.nextDouble();
 
         System.out.print("Nhap so a_21: ");
-        int a_21 = scanner.nextInt();
+        double a_21 = scanner.nextDouble();
 
         System.out.print("Nhap so a_22: ");
-        int a_22 = scanner.nextInt();
+        double a_22 = scanner.nextDouble();
 
         System.out.print("Nhap so b_1: ");
-        int b_1 = scanner.nextInt();
+        double b_1 = scanner.nextDouble();
 
         System.out.print("Nhap so b_2: ");
-        int b_2 = scanner.nextInt();
+        double b_2 = scanner.nextDouble();
 
-        int D = a_11 * a_22 - a_21 * a_12;
-        int D1 = b_1 * a_22 - b_2 * a_12;
-        int D2 = a_11 * b_2 - a_21 * b_1;
+        double D = a_11 * a_22 - a_21 * a_12;
+        double D1 = b_1 * a_22 - b_2 * a_12;
+        double D2 = a_11 * b_2 - a_21 * b_1;
 
         if(D == 0){
             System.out.println("Dung voi moi x, y");
