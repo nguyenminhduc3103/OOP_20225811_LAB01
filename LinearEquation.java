bf8c092336bbc635723493d4ea29b8223556ce20
MD31032004 <ngminhduc04@gmail.com>
2024-09-28 11:41:43 +0700

    first commit

---
+++ LinearEquation.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class LinearEquation{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Nhap so a: ");
+        int a = scanner.nextInt();
+
+        System.out.print("Nhap so b: ");
+        int b = scanner.nextInt();
+
+        if(a == 0){
+            if(b == 0){
+                System.out.println("Dung voi moi so.");
+            }
+            else{
+                System.out.println("Khong co x thoa man.");
+            }
+        }
+        else{
+            double x = -b / a;
+            System.out.println("x = " + x);
+        }
+    }
+}
---
+++ LinearTwoVari.java
@@ -0,0 +1,9 @@
+import java.util.scanner;
+
+public class LinearTwoVari{
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Nhap so ")
+    }
+}
